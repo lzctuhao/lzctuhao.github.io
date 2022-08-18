bdd0f32930fcf72b1aca447e498b847a02b4a890
lzctuhao <lzctuhao@outlook.com>
2022-08-18 11:13:03 +0800

    Site updated: 2022-08-18 11:13:01

--- js/alonepage/primary.js
+++ js/alonepage/primary.js
@@ -13,21 +13,6 @@ const Toast = Swal.mixin({
     }
 })
 
-function primary_series(current){
-    Swal.fire({
-        title: '小学练习系列页面',
-        showConfirmButton: false,
-        showCloseButton: true,
-        html: ' <div id="primary_series" class="collection">\
-        <a href="/tools/primary/" class="collection-item">最大公约数练习</a>\
-        <a href="/tools/primary2/" class="collection-item">两位数除以一位数</a>\
-        <a href="/2022/0814/folder-ganhuo/xiao-liu-ying-yu/" class="collection-item">六上单词</a>\
-      </div>',
-    })
-    if(current>0){ /*标记当前页*/
-        $("#primary_series").find('a').eq(current-1).append('<span class="badge">当前</span>');
-    }
-}
 
 function randint(n,m){/*范围包括n和m*/
     return parseInt(Math.random()*(m-n+1)+n)
